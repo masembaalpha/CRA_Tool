cbe983881ed2733005e0b84b3620c06ee56064af
masembaalpha <149681590+masembaalpha@users.noreply.github.com>
2024-05-24 16:37:02 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,10 @@
+# creditscorecardbuilder
+
+This application is a Web-based interface to develop credit risk scorecards. It is a fairly basic application but works for simple use cases. It supports
+
+* Data Upload
+* Exploratory Data Analysis
+* Sampling
+* Binning (Weight of Evidence and Information Value calculation)
+* Scorecard Development and Alignment
+* Scorecard Validation
---
+++ codes/libs.R
@@ -0,0 +1,19 @@
+# Packages ----------------------------------------------------------------
+
+library(shiny)
+library(shinythemes)
+library(rhandsontable)
+library(readr)
+library(knitr)
+library(summarytools)
+library(ggplot2)
+library(dplyr)
+library(smbinning)
+library(tidyr)
+library(car)
+library(plotly)
+
+# Options -----------------------------------------------------------------
+
+options(shiny.maxRequestSize = Inf)
+options(shiny.minified = TRUE)
